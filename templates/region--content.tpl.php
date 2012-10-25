138b6a2388cfbc4a5d3bfad5750cdd01bfb113ce
Chris W. Gross <gross.cw@gmail.com>
2012-10-25 10:47:24 -0400

    added content_with_tabs div

--- templates/region--content.tpl.php
+++ templates/region--content.tpl.php
@@ -10,7 +10,9 @@
     <?php print render($title_suffix); ?>
     <?php if ($tabs && !empty($tabs['#primary'])): ?><div class="tabs clearfix"><?php $primary = $tabs; unset($primary['#secondary']); print render($primary); ?></div><?php endif; ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-    <?php $secondary = $tabs; unset($secondary['#primary']); print render($secondary); print $content; ?>
+    <div id="content_and_tabs">
+      <?php $secondary = $tabs; unset($secondary['#primary']); print render($secondary); print $content; ?>
+    </div>
     <?php if ($feed_icons): ?><div class="feed-icon clearfix"><?php print $feed_icons; ?></div><?php endif; ?>
   </div>
 </div>
